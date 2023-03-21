5e7bcdb9e131608d3bd2c06e68d61680915c9067
MasterSkvidward <masterskvidvard@gmail.com>
2023-03-22 00:20:54 +0300

    logo

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,11 +1,14 @@
 import React, {FC} from 'react';
 import classes from '../../styles/Navbar.module.scss';
 import { Link } from 'react-router-dom';
+import logo from '../../images/car_logo.png';
 
 const Navbar:FC= () => {
     return (
        <div className={[classes.navbar, '_container'].join(' ')}>
-           <div className={classes.navbar__logo}><img src="" alt="" title='Car' /><span className={classes.navbar__title}>Car Rental</span></div>
+           <div className={classes.navbar__logo}>
+                <img src={logo} alt="" title='Car' /><span className={classes.navbar__title}>Car Rental</span>
+            </div>
            <div className={''}>
                 <ul className={classes.navbar__links} >
                     <li className={classes.navbar__link}>Автомобили<Link to=''></Link></li>
